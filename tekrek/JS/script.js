267030f839a09e9d59217442ee56f1fa02ed423e
LIlith07 <liana.teculescu@gmail.com>
2018-06-30 11:06:29 +0200

    Initial commit

---
+++ tekrek/JS/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    /* navbar toggler */
+
+    $('.navbar-toggler').html("<i class='fas fa-angle-down fa-3x' style='color:#32A3AD'></i>");
+
+    // smooth scroll
+
+});
+
